1e5ee900eb4d6981ace5b1aaa8b6a975ff1ee325
Anatolii Balakiriev <dreamsuccesslife@gmail.com>
2023-12-11 16:02:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ languages = ["russian", "ukrainian", "english", "polish", "turkish", "italian", 
 
 language = st.sidebar.radio("Select Language", languages)
 
+# Перевірка, чи існує файл для обраної мови
+language_file_path = os.path.join("language", f"{language}.py")
+if os.path.isfile(language_file_path):
+    exec(open(language_file_path).read())
+else:
+    st.write(f"No code found for language: {language}")
+
 # Очищення поля вводу при зміні мови
 if 'language' not in st.session_state:
     st.session_state.language = ''
