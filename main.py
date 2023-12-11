5a54d38039c7c5eec96048c712a239cade29d4a4
Anatolii Balakiriev <dreamsuccesslife@gmail.com>
2023-12-11 14:50:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import math
 # Інші імпорти
 
 language = st.sidebar.selectbox("Select Language", ["russian", "ukrainain", "english", "polish", "turkish", "italian", "dutch",
-                                                    "serbian", "german", "czech", "french", "bolgarian", "spanish", "romanian"])
+                                                    "serbian", "german", "czech", "french", "bulgarian", "spanish", "romanian"])
 
 # Перевірка, чи існує файл для обраної мови
 language_file_path = os.path.join("language", f"{language}.py")
