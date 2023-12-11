4c7b0a0d565564f83618579480f5d2e6f19cb786
Anatolii Balakiriev <dreamsuccesslife@gmail.com>
2023-12-11 15:38:00 +0200

    Update russian.py

--- language/russian.py
+++ language/russian.py
@@ -198,3 +198,24 @@ def get_top_2_relevant_articles(conn, query):
         top_2_relevant_articles = cursor.fetchall()
 
     return top_2_relevant_articles
+    
+st.title("Fake Detection")
+st.sidebar.header("Input")
+
+input_string = st.sidebar.text_input("Input your request and press the RUN button or press Enter", height=100)
+
+if st.sidebar.button("RUN"):
+    # Отримання оброблених документів з бази даних
+    connection = create_database_connection()
+    
+    if connection:
+        top_2_relevant_articles = get_top_2_relevant_articles(connection, input_string)
+    
+        # Виведення результатів
+        for article_id, article, cosine_similarity in top_2_relevant_articles:
+            st.write(f"\n\nThe article with id {article_id} has a similarity value {cosine_similarity}\n\n The Article:\n\n\n{article}")
+    close_database_connection(connection)
+
+# Очистити поле вводу при зміні мови
+if st.sidebar.button("Clear Input"):
+    input_string = ""
