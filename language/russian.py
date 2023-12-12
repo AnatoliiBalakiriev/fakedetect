b3521bf3a8432e0ae14d19c9147af7a3c6ebda01
Anatolii Balakiriev <dreamsuccesslife@gmail.com>
2023-12-12 11:05:28 +0200

    Update russian.py

--- language/russian.py
+++ language/russian.py
@@ -191,7 +191,7 @@ def get_top_2_relevant_articles(conn, query):
                 article,
                 1 - (embed <=> (SELECT query_vector FROM request)) AS cosine_similarity,
                 url,
-                relative_urls
+                relative_images
             FROM pgml.stopfakes_ru
             ORDER BY cosine_similarity DESC
             LIMIT 2;
