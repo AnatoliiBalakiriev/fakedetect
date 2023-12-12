160b97572570fd498383ed3557843e3792535c54
Anatolii Balakiriev <dreamsuccesslife@gmail.com>
2023-12-12 11:06:01 +0200

    Update russian.py

--- language/russian.py
+++ language/russian.py
@@ -231,6 +231,6 @@ if st.sidebar.button("RUN"):
 
             # Вивести посилання на зображення
             for image_link in image_links:
-                st.markdown(image_link)
+                st.image(image_link)
                 
     close_database_connection(connection)
