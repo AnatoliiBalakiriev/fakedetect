32b927fe46c1718a56ad5bdebf59a5c5185cab36
Anatolii Balakiriev <dreamsuccesslife@gmail.com>
2023-12-14 17:27:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 import os
 import streamlit as st
 
+# st.sidebar.selectbox  - anotehr option
 languages = ["russian", "ukrainian", "english", "polish", "turkish", "italian", "dutch",
             "serbian", "german", "czech", "french", "bulgarian", "spanish", "romanian"]
 
-selected_language = st.sidebar.empty()  # Створення місця для вибору мови
-
-# Створення кнопок для вибору мови
-for lang in languages:
-    if st.sidebar.button(lang):
-        selected_language.text(lang)  # Встановлення вибраної мови
-
-language = selected_language.text
+language = st.sidebar.radio("Select Language", languages)
 
 # Перевірка, чи існує файл для обраної мови
-if language:
-    language_file_path = os.path.join("language", f"{language}.py")
-    if os.path.isfile(language_file_path):
-        exec(open(language_file_path).read())
-    else:
-        st.write(f"No code found for language: {language}")
+language_file_path = os.path.join("language", f"{language}.py")
+if os.path.isfile(language_file_path):
+    exec(open(language_file_path).read())
 else:
-    st.write("Please select a language")
+    st.write(f"No code found for language: {language}")
