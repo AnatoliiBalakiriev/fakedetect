777e6f3920d77e30582744cb0bd6306a3af015c0
Anatolii Balakiriev <dreamsuccesslife@gmail.com>
2023-12-14 17:15:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import streamlit as st
 # Введення даних та кнопка "RUN"
 st.sidebar.header("Input")
 input_string = st.sidebar.text_area("Input your request and press the RUN button or press Enter", height=100)
-run_pressed = st.sidebar.button("RUN")
+st.sidebar.button("RUN")
 
 # Вибір мови
 languages = ["russian", "ukrainian", "english", "polish", "turkish", "italian", "dutch",
             "serbian", "german", "czech", "french", "bulgarian", "spanish", "romanian"]
 language = st.sidebar.radio("Select Language", languages)
 
-# Перевірка, чи була натиснута кнопка "RUN" і чи існує файл для обраної мови
-if run_pressed:
-    language_file_path = os.path.join("language", f"{language}.py")
-    if os.path.isfile(language_file_path):
-        exec(open(language_file_path).read())
-    else:
-        st.write(f"No code found for language: {language}")
+language_file_path = os.path.join("language", f"{language}.py")
+if os.path.isfile(language_file_path):
+            exec(open(language_file_path).read())
+else:
+            st.write(f"No code found for language: {language}")
