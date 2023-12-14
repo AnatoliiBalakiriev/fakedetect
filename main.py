928525b400f6c52cdf6bf655a923303ca512d3e5
Anatolii Balakiriev <dreamsuccesslife@gmail.com>
2023-12-14 16:43:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import streamlit as st
 languages = ["russian", "ukrainian", "english", "polish", "turkish", "italian", "dutch",
             "serbian", "german", "czech", "french", "bulgarian", "spanish", "romanian"]
 
-language = st.sidebar.radio("Select Language", languages)
-
 # Перевірка, чи існує файл для обраної мови
 language_file_path = os.path.join("language", f"{language}.py")
 if os.path.isfile(language_file_path):
     exec(open(language_file_path).read())
 else:
     st.write(f"No code found for language: {language}")
+
+language = st.sidebar.radio("Select Language", languages)
