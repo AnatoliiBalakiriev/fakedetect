79348ef3399dee9206141e9a3061ddfa86f0cf81
Anatolii Balakiriev <dreamsuccesslife@gmail.com>
2023-12-14 17:31:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import os
 import streamlit as st
 
-# st.sidebar.selectbox  - anotehr option
 languages = ["russian", "ukrainian", "english", "polish", "turkish", "italian", "dutch",
             "serbian", "german", "czech", "french", "bulgarian", "spanish", "romanian"]
 
-language = st.sidebar.radio("Select Language", languages)
+# Відобразити поле вводу
+st.title("Fake Detection")
+st.sidebar.header("Input")
+input_string = st.sidebar.text_area("Input your request and press the RUN button or press Enter", height=100)
 
-# Перевірка, чи існує файл для обраної мови
+# Відобразити вибір мови
+language = st.sidebar.radio("Select Language", languages)
 language_file_path = os.path.join("language", f"{language}.py")
+
 if os.path.isfile(language_file_path):
     exec(open(language_file_path).read())
 else:
