1008c0d34948eb730517a22aeaba3354a6248153
Anatolii Balakiriev <dreamsuccesslife@gmail.com>
2023-12-14 17:38:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ st.title("Fake Detection")
 st.sidebar.header("Input")
 input_string = st.sidebar.text_area("Input your request and press the RUN button or press Enter", height=100)
 st.sidebar.button("RUN")
-if st.sidebar.button("RUN"):
 
-            # Відобразити вибір мови
-            language = st.sidebar.radio("Select Language", languages)
-            language_file_path = os.path.join("language", f"{language}.py")
-            
-            if os.path.isfile(language_file_path):
-                exec(open(language_file_path).read())
-            else:
-                st.write(f"No code found for language: {language}")
+# Відобразити вибір мови
+language = st.sidebar.radio("Select Language", languages)
+language_file_path = os.path.join("language", f"{language}.py")
+
+if os.path.isfile(language_file_path):
+    exec(open(language_file_path).read())
+else:
+    st.write(f"No code found for language: {language}")
